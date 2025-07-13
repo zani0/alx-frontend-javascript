d286984f3117ae49926545cd273667fbc7362e8b
zani0 <106967971+zani0@users.noreply.github.com>
2025-07-13 18:49:45 +0000

    Added task_1

---
+++ task_1/js/main.ts
@@ -0,0 +1,36 @@
+// Teacher interface
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any; 
+}
+
+// Sample Teacher object
+const teacher1: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: false,
+  location: 'London',
+  contract: false,
+};
+
+console.log('Teacher:', teacher1);
+
+// Directors interface extending Teacher
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Sample Director object
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: true,
+  location: 'London',
+  numberOfReports: 17,
+};
+
+console.log('Director:', director1);
